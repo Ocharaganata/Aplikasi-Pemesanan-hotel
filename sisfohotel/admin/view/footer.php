6fc7a31203a52d4dd7cf8f67839734ac882fdf79
ocha <kesumaocha30@gmail.com>
2024-01-22 13:25:43 +0700

    first commit

---
+++ sisfohotel/admin/view/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- <title>Document</title> -->
+	<!-- <link rel="stylesheet" href="css/style.css"> -->
+</head>
+<body>
+	<footer>
+		<h6>Copyright &copy; 2022 | Repost by <a href='Ocha Raganata' title='Ocha Raganata' target='_blank'>Ocha Raganata</a>
+		</h6>
+	</footer>
+</body>
+</html>
